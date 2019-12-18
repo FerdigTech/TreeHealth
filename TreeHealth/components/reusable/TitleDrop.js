f79a2b46a62df1617e749d0ebe23dfa507965081
Chris Lenart <clenart4@kent.edu>
2019-12-18 17:03:02 -0500

    Added dropdown

--- TreeHealth/components/reusable/TitleDrop.js
+++ TreeHealth/components/reusable/TitleDrop.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StyleSheet, View, TouchableOpacity } from "react-native";
 import { Icon, Text } from "native-base";
+import { List, ListItem } from "native-base";
 
 export class TitleDrop extends React.Component {
   constructor(props) {
@@ -25,6 +26,7 @@ export class TitleDrop extends React.Component {
             style={styles.dropIcon}
           />
         </View>
+        {this.state.currentlyDropd && <ScrollDrop />}
       </TouchableOpacity>
     );
   }
@@ -38,7 +40,35 @@ TitleDrop.propTypes = {
   projectName: PropTypes.string.isRequired
 };
 
+export class ScrollDrop extends React.Component {
+  render() {
+    return (
+      <View style={styles.dropLst}>
+        <List>
+          <ListItem>
+            <Text numberOfLines={1} ellipsizeMode="tail">
+              Project 2
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text>Project 3</Text>
+          </ListItem>
+          <ListItem>
+            <Text>Project 4</Text>
+          </ListItem>
+        </List>
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
+  dropLst: {
+    backgroundColor: "white",
+    position: "absolute",
+    top: 38,
+    zIndex: 2
+  },
   dropContr: {
     marginLeft: "auto",
     marginRight: "auto"
