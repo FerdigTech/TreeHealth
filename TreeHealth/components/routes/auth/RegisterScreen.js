035307dfac6c7154e0aad0a64557d87f394ee597
Chris Lenart <clenart4@kent.edu>
2020-01-01 14:46:46 -0500

    Added styling to register page

--- TreeHealth/components/routes/auth/RegisterScreen.js
+++ TreeHealth/components/routes/auth/RegisterScreen.js
@@ -1,6 +1,18 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, ScrollView } from "react-native";
-import { Button, Text, Form, Item, Input, Label, Container } from "native-base";
+import { SafeAreaView, StyleSheet, ScrollView, View } from "react-native";
+import {
+  Button,
+  Text,
+  Form,
+  Item,
+  Input,
+  Label,
+  Container,
+  DatePicker,
+  Picker,
+  Icon,
+  CheckBox
+} from "native-base";
 import { LogoTitle } from "../../reusable/LogoTitle";
 
 export class RegisterScreen extends React.Component {
@@ -10,34 +22,53 @@ export class RegisterScreen extends React.Component {
   };
 
   render() {
+    const today = new Date();
+    const today_13_yrs_ago = new Date().setFullYear(today.getFullYear() - 13);
     return (
       <SafeAreaView style={styles.signUpView}>
         <ScrollView>
           <Form style={styles.signUpForm}>
-          <Item floatingLabel>
-              <Label>Name</Label>
-              <Input />
+            <Item floatingLabel>
+              <Label style={styles.labels}>Name</Label>
+              <Input style={styles.inputs}/>
             </Item>
             <Item floatingLabel>
-              <Label>Email</Label>
-              <Input />
+              <Label style={styles.labels}>Email</Label>
+              <Input style={styles.inputs}/>
             </Item>
             <Item floatingLabel>
-              <Label>Password</Label>
-              <Input secureTextEntry={true} />
+              <Label style={styles.labels}>Password</Label>
+              <Input style={styles.inputs} secureTextEntry={true} />
             </Item>
-            <Item floatingLabel>
-              <Label>Affiliation</Label>
-              <Input />
+            <Item picker style={styles.pickers}>
+              <Picker
+                note
+                mode="dropdown"
+                iosIcon={<Icon name="arrow-down" />}
+                placeholder="Select Affliation"
+                placeholderStyle={styles.labels}
+                placeholderIconColor="#000"
+              >
+                <Picker.Item label="Affliation 1" value="affliation1" />
+                <Picker.Item label="Affliation 2" value="affliation2" />
+              </Picker>
             </Item>
-            <Item floatingLabel>
-              <Label>Age</Label>
-              <Input />
+            <Item picker style={styles.pickers}>
+              <DatePicker
+                maximumDate={today_13_yrs_ago}
+                locale={"en"}
+                placeHolderText="Select Birthday"
+                placeHolderTextStyle={styles.labels}
+              />
             </Item>
-            <Item floatingLabel>
-              <Label>Request Data Access?</Label>
-              <Input />
+            <Item style={styles.checkboxes}>
+              <Label style={styles.labels}>Request Data Access?</Label>
+              <CheckBox checked={false} color="black"/>
             </Item>
+            <View style={styles.terms}>
+              <Text>I agree to the terms</Text>
+              <CheckBox checked={false} color="black"/>
+            </View>
           </Form>
           <Container style={styles.SignUpBtnCtn}>
             <Button style={styles.SignUpBtn} rounded block light>
@@ -52,7 +83,6 @@ export class RegisterScreen extends React.Component {
 
 const styles = StyleSheet.create({
   signUpView: {
-    flex: 1,
     margin: 20
   },
   signUpForm: {
@@ -61,10 +91,29 @@ const styles = StyleSheet.create({
     flex: 1
   },
   SignUpBtnCtn: {
-    padding: 10,
-    flex: 1
+    padding: 10
   },
   SignUpBtn: {
     margin: 15
+  },
+  labels: {
+    color: "#808080",
+    paddingLeft: 10
+  },
+  pickers: {
+    marginLeft: 15
+  },
+  checkboxes: {
+    paddingTop: 5,
+    paddingBottom: 5
+  },
+  terms: {
+    paddingTop: 15,
+    paddingLeft: 10,
+    flexDirection: 'row',
+    justifyContent: "center"
+  },
+  inputs: {
+    paddingLeft: 10
   }
 });
