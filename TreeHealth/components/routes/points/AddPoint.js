8af7a27e2401056b7433ad8f99eb0cc9a0bc880b
Chris Lenart <clenart4@kent.edu>
2020-01-03 12:32:51 -0500

    Offline mode and warning for permissions

--- TreeHealth/components/routes/points/AddPoint.js
+++ TreeHealth/components/routes/points/AddPoint.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, StyleSheet, SafeAreaView, Text } from "react-native";
-import { Form, Item, Input, Button } from "native-base";
+import {
+  ScrollView,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  ActivityIndicator
+} from "react-native";
+import { Form, Item, Input, Button, Label } from "native-base";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
 import { useNetInfo } from "@react-native-community/netinfo";
@@ -28,28 +35,75 @@ export const AddPoint = () => {
     }
   };
 
+  _updateLatitude = ({ text }) => {
+    let newState = Object.assign({}, location);
+    const input = text.toString();
+    if (input) {
+      newState.coords.latitude = input;
+    } else {
+      newState.coords.latitude = "";
+    }
+    setLocation(newState);
+  };
+
+  _updateLongitude = ({ text }) => {
+    let newState = Object.assign({}, location);
+    const input = text.toString();
+    if (input) {
+      newState.coords.longitude = input;
+    } else {
+      newState.coords.longitude = "";
+    }
+    setLocation(newState);
+  };
+
   return (
     <SafeAreaView style={styles.AddPtView}>
       <ScrollView>
-        <Text>Type: {netInfo.type}</Text>
-        <Text>Is Connected? {netInfo.isConnected.toString()}</Text>
-        <Form>
-          <Item style={styles.formItem} rounded>
+        {location == null &&
+          netInfo.isConnected && (
+            <View style={styles.loadingView}>
+              <Text style={styles.loadingInstrHeader}>Processing Location</Text>
+              <ActivityIndicator />
+              <Text style={styles.loadingInstr}>
+                If this takes longer than expected, please make sure location is
+                enabled and retry again.
+              </Text>
+              <Text style={styles.loadingInstr}>
+                This is the result of not giving the App permissions.
+              </Text>
+            </View>
+          )}
+        {!netInfo.isConnected && (
+          <View style={styles.offlineView}>
+            <Text style={styles.offlineHeadInstr}>Offline Mode</Text>
+            <Text style={styles.offlineInstr}>
+              The data will be queued to be uploaded when your device appears to
+              have service.
+            </Text>
+          </View>
+        )}
+        <Form style={styles.form}>
+          <Item style={styles.formItem} rounded floatingLabel>
+            <Label style={styles.itemStyle}>Latitude</Label>
             <Input
+              style={styles.itemStyle}
               value={
                 location != null ? location.coords.latitude.toString() : ""
               }
               placeholder="Latitude"
-              onChangeText={e => {}}
+              onChangeText={text => _updateLatitude({ text })}
             />
           </Item>
-          <Item style={styles.formItem} rounded>
+          <Item style={styles.formItem} rounded floatingLabel>
+            <Label style={styles.itemStyle}>Longitude</Label>
             <Input
+              style={styles.itemStyle}
               value={
                 location != null ? location.coords.longitude.toString() : ""
               }
               placeholder="Longitude"
-              onChangeText={e => {}}
+              onChangeText={text => _updateLongitude({ text })}
             />
           </Item>
         </Form>
@@ -68,17 +122,52 @@ export const AddPoint = () => {
 };
 
 const styles = StyleSheet.create({
-  AddPtView: {
-    margin: 10
-  },
+  AddPtView: {},
   formItem: {
     margin: 15,
     padding: 5
   },
+  form: {
+    margin: 10
+  },
+  itemStyle: {
+    padding: 10
+  },
   recordBtn: {
-    marginTop: 10
+    margin: 10
   },
   RecordBtnTxt: {
     color: "white"
+  },
+  loadingView: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  loadingInstr: {
+    fontSize: 18
+  },
+  loadingInstrHeader: {
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+    padding: 10
+  },
+  offlineHeadInstr: {
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "bold",
+    padding: 10,
+    color: "white"
+  },
+  offlineInstr: {
+    fontSize: 18,
+    color: "white"
+  },
+  offlineView: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "red",
+    width: "100%",
+    padding: 10
   }
 });
