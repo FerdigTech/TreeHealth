04530e97b60f233058e6b0e565f8e437b7dad5ed
Chris Lenart <clenart4@kent.edu>
2020-01-04 15:58:21 -0500

    Added a slide view to be able to edit records

--- TreeHealth/components/routes/questions/QuestionItem.js
+++ TreeHealth/components/routes/questions/QuestionItem.js
@@ -1,36 +1,63 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 import { Button, ListItem, Text, Icon, Left, Body, Right } from "native-base";
- 
+import { SwipeRow } from "react-native-swipe-list-view";
 
-export class QuestionItem extends React.Component {
-  render() {
-    return (
-      <ListItem style={styles.listItemStyle}>
-        <Left style={styles.leftHandInfo}>
+export const QuestionItem = props => {
+  return (
+    <SwipeRow key={props.indexVal}
+      style={styles.standalone}
+      disableRightSwipe={true}
+      rightOpenValue={-75}
+    >
+      <View style={styles.rowBack}>
+        <TouchableOpacity
+          style={styles.standaloneRowBack}
+          onPress={() => {}}
+          activeOpacity={0.5}
+        >
+          <Text style={styles.backTextWhite}>Edit</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.standaloneRowFront}>
+        <ListItem style={styles.listItemStyle}>
+          <Left style={styles.leftHandInfo}>
             <Text style={styles.itemDate}>October 14, 2019</Text>
-            <Text style={styles.itemDraft}>{this.props.isDraft ? 'Draft' : ' '}</Text>
-            <Text style={styles.itemUpdated} note>last updated August 3, 2019</Text>
-        </Left>
-        <Body/>
-        <Right>
-          <Button style={styles.lstIconBtn} warning={this.props.isDraft} success={!this.props.isDraft}>
-            <Icon style={styles.listIcon} name={ this.props.isDraft ? 'create' : 'checkmark'} />
-          </Button>
-        </Right>
-      </ListItem>
-    );
-  }
-}
+            <Text style={styles.itemDraft}>
+              {props.isDraft ? "Draft" : " "}
+            </Text>
+            <Text style={styles.itemUpdated} note>
+              last updated August 3, 2019
+            </Text>
+          </Left>
+          <Body />
+          <Right>
+            <Button
+              style={styles.lstIconBtn}
+              warning={props.isDraft}
+              success={!props.isDraft}
+            >
+              <Icon
+                style={styles.listIcon}
+                name={props.isDraft ? "create" : "checkmark"}
+              />
+            </Button>
+          </Right>
+        </ListItem>
+      </View>
+    </SwipeRow>
+  );
+};
 
 QuestionItem.propTypes = {
-  isDraft: PropTypes.bool.isRequired
+  isDraft: PropTypes.bool.isRequired,
+  indexVal: PropTypes.number.isRequired
 };
 
 const styles = StyleSheet.create({
   lstIconBtn: {
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end",
     height: 24,
     width: 24,
     borderRadius: 24,
@@ -38,25 +65,43 @@ const styles = StyleSheet.create({
   },
   listIcon: {
     marginLeft: 0,
-    marginRight: 0, 
-    fontSize: 18 
+    marginRight: 0,
+    fontSize: 18
   },
   itemUpdated: {
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start"
   },
   itemDraft: {
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start"
   },
   itemDate: {
     fontWeight: "bold",
-    alignSelf: 'flex-start',
+    alignSelf: "flex-start"
   },
-  listItemStyle: {
-  },
+  listItemStyle: {},
   leftHandInfo: {
     flexDirection: "column",
     alignItems: "flex-start",
     alignContent: "flex-start",
     textAlign: "left"
+  },
+  standalone: {
+    flex: 1
+  },
+  standaloneRowFront: {
+    backgroundColor: "#f4f4f4",
+    zIndex: 2
+  },
+  standaloneRowBack: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    height: "100%",
+    padding: 20,
+    backgroundColor: "#f0ad4e",
+    zIndex: 2
+  },
+  backTextWhite: {
+    color: "#FFF"
   }
 });
--- TreeHealth/components/routes/questions/QuestionList.js
+++ TreeHealth/components/routes/questions/QuestionList.js
@@ -1,19 +1,37 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import {
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  StyleSheet
+} from "react-native";
 import { QuestionItem } from "./QuestionItem";
-import { Container, Content } from "native-base";
+import { Container, Content, List, Icon, Button } from "native-base";
 
-export class QuestionList extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Content>
-          <ScrollView style={{ flex: 1 }}>
-            <QuestionItem isDraft={true} />
-            <QuestionItem isDraft={false} />
-          </ScrollView>
-        </Content>
-      </Container>
-    );
+export const QuestionList = () => {
+  return (
+    <Container>
+      <Content>
+        <ScrollView style={{ flex: 1 }}>
+        <QuestionItem indexVal={1} isDraft={true} />
+        <QuestionItem indexVal={2} isDraft={false} />
+        </ScrollView>
+      </Content>
+    </Container>
+  );
+};
+
+QuestionList.navigationOptions = ({ navigation }) => ({
+  headerRight: () => (
+    <TouchableOpacity onPress={() => navigation.navigate("AddPoint")}>
+      <Text style={styles.NavText}>Add Record</Text>
+    </TouchableOpacity>
+  )
+});
+
+const styles = StyleSheet.create({
+  NavText: {
+    color: "white",
+    paddingRight: 10
   }
-}
+});
