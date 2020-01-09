daca5310a16569500b181574eec44f0ef8cde794
Chris Lenart <clenart4@kent.edu>
2020-01-09 13:52:18 -0500

    removed font error during dev

--- TreeHealth/App.js
+++ TreeHealth/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { LoadingScreen } from "./components/routes/auth/LoadingScreen";
@@ -107,6 +107,16 @@ const InitalNavigator = createAppContainer(
 );
 
 const App = () => {
+  useEffect(() => {
+    async function setupFonts() {
+      await Expo.Font.loadAsync({
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+      });
+    }
+
+    setupFonts();
+  }, []);
   return (
     <ProjectWrapper>
       <InitalNavigator
