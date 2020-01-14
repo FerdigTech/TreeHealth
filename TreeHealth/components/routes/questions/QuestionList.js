de5e3916f1b79d24787ef6a065071725c43e3d7f
Chris Lenart <clenart4@kent.edu>
2020-01-14 16:04:12 -0500

    Added footer item for items in queue

--- TreeHealth/components/routes/questions/QuestionList.js
+++ TreeHealth/components/routes/questions/QuestionList.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ScrollView, TouchableOpacity, Text, StyleSheet } from "react-native";
 import { QuestionItem } from "./QuestionItem";
-import { Container, Content } from "native-base";
+import { Container, Content, Footer, FooterTab, Button } from "native-base";
+import { FilterModal } from "../../reusable/FilterModal";
+import { ProjectContext } from "../../../context/ProjectProvider";
 
-export const QuestionList = () => {
+export const QuestionList = props => {
+  const context = useContext(ProjectContext);
+  // from the filter
+  const DropDownVisible = props.navigation.getParam("DropDownVisible", false);
+  const Operator = props.navigation.getParam("Operator", "none");
+  const FilterAffilation = props.navigation.getParam("FilterAffilation", false);
+  const OnlyAffilation = props.navigation.getParam("OnlyAffilation", false);
+  const EndDateFilter = props.navigation.getParam("EndDateFilter", "");
+  const dateFilter = props.navigation.getParam("dateFilter", "");
   return (
     <Container>
       <Content>
@@ -11,7 +21,15 @@ export const QuestionList = () => {
           <QuestionItem indexVal={1} isDraft={true} />
           <QuestionItem indexVal={2} isDraft={false} />
         </ScrollView>
+        <FilterModal navigation={props.navigation} />
       </Content>
+      <Footer>
+        <FooterTab style={styles.footerStyle}>
+          <Text style={styles.footerTxt}>
+            You have {context.OfflineQueue.length.toString()} items in offline queue.
+          </Text>
+        </FooterTab>
+      </Footer>
     </Container>
   );
 };
@@ -28,5 +46,16 @@ const styles = StyleSheet.create({
   NavText: {
     color: "white",
     paddingRight: 10
+  },
+  footerTxt: {
+    alignSelf: "center",
+    textAlign: "center",
+    justifyContent: "center",
+    flex: 1
+  },
+  footerStyle: {
+    backgroundColor: "#f4f4f4",
+    borderTopColor: "#ccc",
+    borderTopWidth: 1
   }
 });
