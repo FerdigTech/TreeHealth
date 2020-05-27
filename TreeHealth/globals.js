ebd2ef57ba09f96e4fc0b613b788974c5d95b8a3
Chris <clenart4@kent.edu>
2020-05-27 18:28:47 -0400

    switch URL based on environment

--- TreeHealth/globals.js
+++ TreeHealth/globals.js
@@ -1,5 +1,31 @@
+import Constants from "expo-constants";
+
+const ENV = {
+  dev: {
+    BACKEND_URL: "https://localhost"
+  },
+  staging: {
+    BACKEND_URL: "https://dev.pinchof.tech"
+  },
+  prod: {
+    BACKEND_URL: "https://treehealthapi.cmparks.net"
+  }
+ };
+ 
+ const getEnv = (env = Constants.manifest.releaseChannel) => {
+  if (__DEV__) {
+    return ENV.dev;
+  } else if (env === 'staging') {
+    return ENV.staging;
+  } else if (env === 'prod') {
+    return ENV.prod;
+  }
+ };
+ 
+
+
 export default {
-  SERVER_URL: "https://treehealthapi.cmparks.net",
+  SERVER_URL: getEnv().BACKEND_URL,
   COLOR: {
     DARK_BLUE: "#0f2834",
     GREEN: "#00b374"
